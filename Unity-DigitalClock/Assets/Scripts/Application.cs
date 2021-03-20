e6c5e69937e1e69ae0a5421e537db733dc2dedc5
ddayin <ddayin@gmail.com>
2021-03-20 16:50:49 +0900

    안드로이드 화면 회전 fix

--- Unity-DigitalClock/Assets/Scripts/Application.cs
+++ Unity-DigitalClock/Assets/Scripts/Application.cs
@@ -7,6 +7,8 @@ public class Application : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Screen.orientation = ScreenOrientation.LandscapeRight;
+        
         // Disable screen dimming
         Screen.sleepTimeout = SleepTimeout.NeverSleep;
         

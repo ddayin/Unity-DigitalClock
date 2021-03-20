01560664c98d877247f0f0d7125bba66ef86d259
ddayin <ddayin@gmail.com>
2021-03-20 17:46:42 +0900

    배경 랜덤 전환

--- Unity-DigitalClock/Assets/Scripts/Application.cs
+++ Unity-DigitalClock/Assets/Scripts/Application.cs
@@ -7,18 +7,10 @@ public class Application : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Screen.orientation = ScreenOrientation.LandscapeRight;
-        
         // Disable screen dimming
         Screen.sleepTimeout = SleepTimeout.NeverSleep;
-        
+
         // 배터리 소모 줄이기
         UnityEngine.Application.targetFrameRate = 1;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
---
+++ Unity-DigitalClock/Assets/Scripts/BackgroundImage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackgroundImage : MonoBehaviour
+{
+    public Image image;
+
+    public List<Sprite> bgList;
+
+    private int total = 0;
+    private void Awake()
+    {
+        InvokeRepeating("Change", 60f, 60f);
+        total = bgList.Count;
+    }
+
+    private void Change()
+    {
+        int random = UnityEngine.Random.Range(0, total);
+        image.sprite = bgList[random];
+    }
+}

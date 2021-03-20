7c9263c178f9737d85205cddfbe4353063dd8468
ddayin <ddayin@gmail.com>
2021-03-20 16:07:26 +0900

    시계 틀 잡기

--- Unity-DigitalClock/Assets/Scripts/Application.cs
+++ Unity-DigitalClock/Assets/Scripts/Application.cs
@@ -7,7 +7,8 @@ public class Application : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Disable screen dimming
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
     }
 
     // Update is called once per frame
